e28c78ab909e4d7c6e3c9808e62d9a0e0d441426
mayukorin <19t7b2fx@gmail.com>
2020-05-07 22:28:04 +0900

    遷移先に戻ってくる時に呼ばれるメソッドunwind を作成し、「戻る」ボタンを押すと、前の画面に遷移するようにした。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
